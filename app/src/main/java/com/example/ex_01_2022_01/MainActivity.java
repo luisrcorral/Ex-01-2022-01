dfba3127b15e4eb4dde49c00f3c7a2509bf21709
luisrcorral <luisrcorral@gmail.com>
2022-03-02 14:27:09 -0600

    Initial baseline

---
+++ app/src/main/java/com/example/ex_01_2022_01/MainActivity.java
@@ -0,0 +1,65 @@
+// Example 1 - TC2024 2022
+// By Luis Corral
+
+// This is the package we defined in the app wizard
+package com.example.ex_01_2022_01;
+
+// These are the imports that are required to bring on several classes used in our code
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+// This is the definition of the activity class. The name must be the same than our file
+// name (MainActivity.java). Please notice that this class inherits (extends) AppCompatActivity
+public class MainActivity extends AppCompatActivity {
+
+    // The following attributes are the definition of our object managers. They correspond to the
+    // views (widgets) defined in our layout (XML) files. Be mindful that defining and using
+    // these objects require the inclusion of the proper library (see above section "imports")
+    private Button buttonOM;
+    private EditText editTextOM;
+    private TextView textViewOM;
+
+    // The override annotation tells the compiler to consider this and more onCreate methods.
+    // According to the activity lifecycle, onCreate is the first method executed when the
+    // activity is launched.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // This method, member of the AppCompatActivity class, renders the content of the
+        // layout expressed in the parameter.
+        setContentView(R.layout.activity_main);
+
+        // In the following lines, we assign the object manager to the effective widget in the
+        // layout file. Always consider that views in the XML file bear a name. This name is
+        // used here to create the association between the XML file and the java Object manager.
+        buttonOM =  findViewById(R.id.myButton);
+        editTextOM = findViewById(R.id.myInputText);
+        textViewOM = findViewById(R.id.myTextView);
+
+        // Using the button object manager, we declare here an event listener to the onClick event
+        buttonOM.setOnClickListener(new View.OnClickListener() {
+
+            // Many views may hold an onClick method. For this reason, the override
+            // annotation is needed
+            @Override
+            // This is the method that effectively defines what the view does when clicked
+            public void onClick(View view) {
+               String myTxt = "";
+               // When the button is clicked, we extract the value of the EditText view, and since
+               // the getText method returns an Editable object, we do the toString callback.
+               myTxt = editTextOM.getText().toString();
+
+               // Due to encapsulation principles, if we want to assign a value to the attribute of
+               // an object, we use a method instead.
+               textViewOM.setText(myTxt);
+
+            }
+        });
+    }
+}
